d3bd7ad1cef503deb1067b0f8bc8666a79f9aa99
essile <essi.lehtola@hotmail.com>
2020-07-02 11:05:49 +0300

    refactoring

--- AsiakastietoGet.cs
+++ AsiakastietoGet.cs
@@ -21,6 +21,7 @@ namespace AsiakastietoApi
             string userId = ValidateHeader("UserId", req, headerError);
             string password = ValidateHeader("Password", req, headerError);
             string checkSumkey = ValidateHeader("CheckSumKey", req, headerError);
+            string endUser = "ccccc";
 
             if (headerError.Count > 0)
                 return new ObjectResult("ERROR: " + String.Join(" ", headerError.ToArray())) { StatusCode = 400 };
@@ -28,26 +29,10 @@ namespace AsiakastietoApi
             string demoUrl = "https://demo.asiakastieto.fi/services/company5/REST";
             string prodUrl = "https://www.asiakastieto.fi/services/company5/REST";
 
-            string dateFormat = "yyyyMMddHHmmssff";
-            var timestamp = DateTime.UtcNow.ToString(dateFormat); // YYYYMMDDHHMMSSXX
-            string offset = "+02"; //time zone correction in relation to GMT. In Finland always "+02". Daylight saving time is not added.
-            string consecutiveNumber = "00000"; // from Asiakastieto, this should work in most cases
-            string asiakastietoTimestamp = timestamp + offset + consecutiveNumber;
-            string endUser = "ccccc";
+            var checksum = FormatChecksum(userId, endUser, checkSumkey, log);
+            log.LogInformation("Checksum: " + checksum);
 
-            string checksumString = $"{userId}&{endUser}&{asiakastietoTimestamp}&{checkSumkey}&";
-
-            // checksumStringExample = "123456123456&ccccc&2020010111000000+0200000&9Gk487z6qBC48R27hpq6RBPoS1hWt88Z755Ku7ub5M5NE08HRj2Mt7KOQhtL0spr&";
-            // The one above should produce checksum 442B66F745DE4CAF0A1E6DC551C9C676205498C7CDF28036DB2229573A12D71C14F13430A1E34D6B4CAF1360E9573931019A7DACB27178D5998B97F4301D54EE
-
-            var checksum = "";
-            using (SHA512 shaM = new SHA512Managed())
-            {
-                var hash = shaM.ComputeHash(Encoding.UTF8.GetBytes(checksumString));
-                checksum = GetStringFromHash(hash);
-            }
-
-            return new OkObjectResult($"checksumstring: {checksumString} \nhash: {checksum}");
+            return new OkObjectResult($"hash: {checksum}");
         }
 
         private static string ValidateHeader(string headerKey, HttpRequest req, List<string> headerError)
@@ -58,6 +43,27 @@ namespace AsiakastietoApi
             {
                 headerError.Add($"Header '{headerKey}' missing.");
                 return "";
+            }
+        }
+
+        private static object FormatChecksum(string userId, string endUser, string checkSumkey, ILogger log)
+        {
+            string dateFormat = "yyyyMMddHHmmssff";
+            var timestamp = DateTime.UtcNow.ToString(dateFormat); // YYYYMMDDHHMMSSXX
+            string offset = "+02"; //time zone correction in relation to GMT. In Finland always "+02". Daylight saving time is not added.
+            string consecutiveNumber = "00000"; // from Asiakastieto, this should work in most cases
+            string asiakastietoTimestamp = timestamp + offset + consecutiveNumber;
+
+            string checksumString = $"{userId}&{endUser}&{asiakastietoTimestamp}&{checkSumkey}&";
+            log.LogInformation("Checksum string: " + checksumString);
+
+            // checksumStringExample = "123456123456&ccccc&2020010111000000+0200000&9Gk487z6qBC48R27hpq6RBPoS1hWt88Z755Ku7ub5M5NE08HRj2Mt7KOQhtL0spr&";
+            // The one above should produce checksum 442B66F745DE4CAF0A1E6DC551C9C676205498C7CDF28036DB2229573A12D71C14F13430A1E34D6B4CAF1360E9573931019A7DACB27178D5998B97F4301D54EE
+
+            using (SHA512 shaM = new SHA512Managed())
+            {
+                var hash = shaM.ComputeHash(Encoding.UTF8.GetBytes(checksumString));
+                return GetStringFromHash(hash);
             }
         }
 
