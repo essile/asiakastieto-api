b0bd236a92147ec3989df9005730770fc9219e32
essile <essi.lehtola@hotmail.com>
2020-07-02 10:59:18 +0300

    creating hash, userid etc coming in as headers

--- AsiakastietoGet.cs
+++ AsiakastietoGet.cs
@@ -1,35 +1,74 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace AsiakastietoApi
 {
     public static class AsiakastietoGet
     {
         [FunctionName("AsiakastietoGet")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            List<string> headerError = new List<string>();
+            string userId = ValidateHeader("UserId", req, headerError);
+            string password = ValidateHeader("Password", req, headerError);
+            string checkSumkey = ValidateHeader("CheckSumKey", req, headerError);
 
-            string name = req.Query["name"];
+            if (headerError.Count > 0)
+                return new ObjectResult("ERROR: " + String.Join(" ", headerError.ToArray())) { StatusCode = 400 };
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string demoUrl = "https://demo.asiakastieto.fi/services/company5/REST";
+            string prodUrl = "https://www.asiakastieto.fi/services/company5/REST";
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string dateFormat = "yyyyMMddHHmmssff";
+            var timestamp = DateTime.UtcNow.ToString(dateFormat); // YYYYMMDDHHMMSSXX
+            string offset = "+02"; //time zone correction in relation to GMT. In Finland always "+02". Daylight saving time is not added.
+            string consecutiveNumber = "00000"; // from Asiakastieto, this should work in most cases
+            string asiakastietoTimestamp = timestamp + offset + consecutiveNumber;
+            string endUser = "ccccc";
 
-            return new OkObjectResult(responseMessage);
+            string checksumString = $"{userId}&{endUser}&{asiakastietoTimestamp}&{checkSumkey}&";
+
+            // checksumStringExample = "123456123456&ccccc&2020010111000000+0200000&9Gk487z6qBC48R27hpq6RBPoS1hWt88Z755Ku7ub5M5NE08HRj2Mt7KOQhtL0spr&";
+            // The one above should produce checksum 442B66F745DE4CAF0A1E6DC551C9C676205498C7CDF28036DB2229573A12D71C14F13430A1E34D6B4CAF1360E9573931019A7DACB27178D5998B97F4301D54EE
+
+            var checksum = "";
+            using (SHA512 shaM = new SHA512Managed())
+            {
+                var hash = shaM.ComputeHash(Encoding.UTF8.GetBytes(checksumString));
+                checksum = GetStringFromHash(hash);
+            }
+
+            return new OkObjectResult($"checksumstring: {checksumString} \nhash: {checksum}");
+        }
+
+        private static string ValidateHeader(string headerKey, HttpRequest req, List<string> headerError)
+        {
+            if (req.Headers.TryGetValue(headerKey, out var headerValue))
+                return headerValue.FirstOrDefault();
+            else
+            {
+                headerError.Add($"Header '{headerKey}' missing.");
+                return "";
+            }
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+            return result.ToString();
         }
     }
 }
